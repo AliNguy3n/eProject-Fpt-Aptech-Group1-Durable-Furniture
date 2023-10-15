03fe8cd05e2765e88b9403d51aa5e6971858671c
Linh Duc <linhnguyenduc.kts@gmail.com>
2023-10-15 21:20:06 +0700

    version3.07

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,6 +6,27 @@ import New from './New'
 import Discover from './Discover'
 
 function Home({handleAddComp,handleCarts}) {
+      const options = {
+        enableHighAccuracy: true, 
+        timeout: 5000, 
+        maximumAge: 2000, 
+    };
+    navigator.geolocation.watchPosition(success, error, options);
+    function success(pos) {
+    const lat = pos.coords.latitude;
+    const lng = pos.coords.longitude;
+    const accuracy = pos.coords.accuracy; // Accuracy in metres
+      console.log('latitude',lat)
+      console.log('longitude',lng)
+      console.log('accuracy',accuracy)
+    }
+    function error(err) {
+    if (err.code === 1) {
+    console.log("Please allow geolocation access");
+    } else {
+    console.log("Cannot get current location");
+    }
+}
   return (
     <div className='home'>
       <div className="home-preview">
